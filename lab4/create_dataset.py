33c1b8dcf27ade6b72bbbfcacbd0355ee1d6afc2
shuher237 <90762786+shuher237@users.noreply.github.com>
2023-09-15 01:42:48 +0300

    Add files via upload

---
+++ lab4/create_dataset.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import os
+from catboost.datasets import titanic
+
+
+titanic_train, titanic_test = titanic()
+
+titanic_df = pd.concat([titanic_train, titanic_test])
+titanic_df.to_csv('./datasets/titanic_df.csv')
+print(titanic_df.head(5))
+ 
---
+++ lab4/modify_dataset.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import os
+
+#path variable
+path = './datasets/titanic_df.csv'
+
+
+#modify dataset
+df = pd.read_csv(path)
+df.drop(df.columns[0], axis=1, inplace= True)
+df.to_csv(path)
+
+print(df.columns.values.tolist())
+
